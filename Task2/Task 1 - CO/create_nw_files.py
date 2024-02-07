d3327f5dff9eb8146d41a8b029ea51d717e690dd
Christian Elias Anderssen Dalan <ceadyy@gmail.com>
2024-02-07 18:25:22 +0100

    Updated radii and nw file creators

--- Task2/Task 1 - CO/create_nw_files.py
+++ Task2/Task 1 - CO/create_nw_files.py
@@ -1,28 +1,30 @@
 import os
-def createFile(radius):
-    myExamplefile = open("CO_example.txt", "r")
+def createNwFiles(radii):
+    myExampleFile = open("CO_example.txt", "r")
 
-    try:
-        folderPath = "CO_" + str(radius)
-        os.mkdir(folderPath)
-    except FileExistsError:
-        pass
+    for radius in radii:
+        try:
+            folderPath = "CO_" + str(radius)
+            os.mkdir(folderPath)
+        except FileExistsError:
+            print("Path already exists, or does it?")
+            pass
 
-    #Replace this with the absolute path of your parent folder
-    filePath = "/Users/elias/Documents/NTNU/V2024/TKJ4170/TKJ4170/Task2/Task 1 - CO/" + folderPath + "/CO_" + str(radius) + ".nw" 
-    myFile = open(filePath, "w")
+        filePath = "/Users/elias/Documents/NTNU/V2024/TKJ4170/TKJ4170/Task2/Task 1 - CO/" + folderPath + "/CO_" + str(radius) + ".nw"
+        myFile = open(filePath, "w")
 
-    for line in myExamplefile:
-        if line.split() == []:
-            myFile.write(line)
-        elif line.split()[0] == "O":
-            print("Line:" + line + "\n")
+        for line in myExampleFile:
+            if line.split() == []:
+                myFile.write(line)
+            elif line.split()[0] == "O":
 
-            words = line.split()
-            words[3] = str(radius)
+                words = line.split()
+                words[3] = str(radius)
 
-            line = " " +  " ".join(words)
-            myFile.write(line + "\n")
-        else:
-            myFile.write(line)
-    myFile.close()
+                line = " " +  " ".join(words)
+                myFile.write(line + "\n")
+            else:
+                myFile.write(line)
+        myFile.close()
+    myExampleFile.close()
+        
--- Task2/Task 1 - CO/create_radii_file.py
+++ Task2/Task 1 - CO/create_radii_file.py
@@ -1,11 +1,12 @@
-radii = [0.75,0.85,0.95,1.0,1.10,1.20,1.30,1.40,1.50,1.60,1.70]
+def createRadiiFile():
+    radiiFile = open("radii.txt", "w")
 
-radiiFile = open("radii.txt", "w")
+    radii = [0.75,0.85,0.95,1.0,1.10,1.20,1.30,1.40,1.50,1.60,1.70]
 
-for radius in radii:
-    if radius != radii[-1]:
-        radiiFile.write(str(radius) + ",")
-    else:
-        radiiFile.write(radius)
+    for radius in radii:
+        if radius != radii[-1]:
+            radiiFile.write(str(radius) + ",")
+        else:
+            radiiFile.write(str(radius))
 
-radiiFile.close()
+    radiiFile.close()
