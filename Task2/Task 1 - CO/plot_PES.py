0ce2ebe753177be8a5bc4ff257daf9e79427c47e
Christian Elias Anderssen Dalan <ceadyy@gmail.com>
2024-03-26 14:06:35 +0100

    Added all of task2

--- Task2/Task 1 - CO/plot_PES.py
+++ Task2/Task 1 - CO/plot_PES.py
@@ -9,5 +9,27 @@ energies = [float(e) for e in energies]
 print(radii)
 print(energies)
 
-plt.plot(radii, energies)
+newRadii = []
+newEnergies = []
+
+for r, e in zip(radii, energies):
+    if (r > 2):
+        break 
+    else:
+        newRadii.append(r)
+        newEnergies.append(e)
+
+minR = newRadii[newEnergies.index(min(newEnergies))]
+minE = min(newEnergies)
+
+
+plt.scatter([minR], [minE], label="Lowest point of energy", color="red", zorder=100)
+plt.plot(newRadii, newEnergies, label="E(R)")
+plt.annotate(f"({minR},{round(minE, 3)})", xy=(minR - 0.1, minE + 0.05))
+plt.title("$CO$ potential energy surface,  RHF/cc-pVDZ")
+plt.xlabel("Radius (Å)")
+plt.ylabel("Energy (Hartree)")
+plt.grid()
+plt.legend()
+plt.savefig("CO_PES.png")
 plt.show()
